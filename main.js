e885a9f142df6bba40ed96e8408f1d26a1020bc2
Keith Alexander <keithalexander@keithalexander.co.uk>
2018-02-28 17:06:10 +0000

    prettified with prettier

--- main.js
+++ main.js
@@ -7,96 +7,109 @@
 // }
 // Path:: [{property: String, optional: Boolean, inverse: Boolean}]
 //
-module.exports = function sparqlTable (spec){
-    const {clauses, columns} = view(spec.views)
-    const {limit, offset} = page(spec.page)
-    const sortClause = spec.sorts.map(({path}, i) => pathProperties(path, '?_sortValue'+i, '')).join('\n')
-    const orderBy = spec.sorts.length?
-       `ORDER BY ${spec.sorts.map(({order}, i) => `${order}(?_sortValue${i})`).join('\t')}`
+module.exports = function sparqlTable(spec) {
+  const {clauses, columns} = view(spec.views)
+  const {limit, offset} = page(spec.page)
+  const sortClause = spec.sorts
+    .map(({path}, i) => pathProperties(path, '?_sortValue' + i, ''))
+    .join('\n')
+  const orderBy = spec.sorts.length
+    ? `ORDER BY ${spec.sorts
+        .map(({order}, i) => `${order}(?_sortValue${i})`)
+        .join('\t')}`
     : ''
-    const groupBy = 
-        spec.views.some(x=>x.funcs.length)?
-        'GROUP BY ' + ['?item'].concat(
-            spec.views.filter(x=>!x.hasOwnProperty('funcs')).map(x=>varize(x.name))
-        ).join(' ')
+  const groupBy = spec.views.some(x => x.funcs.length)
+    ? 'GROUP BY ' +
+      ['?item']
+        .concat(
+          spec.views
+            .filter(x => !x.hasOwnProperty('funcs'))
+            .map(x => varize(x.name)),
+        )
+        .join(' ')
     : ''
 
-    return `${prefixes(spec.namespaces)}
+  return `${prefixes(spec.namespaces)}
 SELECT ?item ${columns.join(' ')}
 WHERE {
- ${spec.filters.map(f => 
-    [f.uris
-     .map(filterUris)
-     .join('\n') 
-    ,
-    f.literals
-     .map(filterLiteral)
-     .join('\n')
-    ].join('\n\t')
- ).map(s => `{ ${s} }`).join(' UNION ')} 
+ ${spec.filters
+   .map(f =>
+     [
+       f.uris.map(filterUris).join('\n'),
+       f.literals.map(filterLiteral).join('\n'),
+     ].join('\n\t'),
+   )
+   .map(s => `{ ${s} }`)
+   .join(' UNION ')} 
  
  ${clauses}
  ${sortClause} 
 }
 ${groupBy}
 ${orderBy}
-${limit >=1? `LIMIT ${limit}`: ''}
-${offset >=0? `OFFSET ${offset}`: ''}
+${limit >= 1 ? `LIMIT ${limit}` : ''}
+${offset >= 0 ? `OFFSET ${offset}` : ''}
 `
 }
 
 function page({number, size}) {
-    const limit = size
-    const offset = number * size - size
-    return {limit, offset}
+  const limit = size
+  const offset = number * size - size
+  return {limit, offset}
 }
 
-
-function prefixes(namespaces){
-    return Object.keys(namespaces)
-        .map(prefix => `PREFIX ${prefix}: <${namespaces[prefix]}>`)
-        .join('\n')
+function prefixes(namespaces) {
+  return Object.keys(namespaces)
+    .map(prefix => `PREFIX ${prefix}: <${namespaces[prefix]}>`)
+    .join('\n')
 }
 
-function view(views){
-   const columns = views.map(columnize) 
-   const clauses = views
-        .map(x => pathProperties(x.path, varizeWithFunctions(x.name, x.funcs), '' ))
-        
-   return  {columns, clauses} 
+function view(views) {
+  const columns = views.map(columnize)
+  const clauses = views.map(x =>
+    pathProperties(x.path, varizeWithFunctions(x.name, x.funcs), ''),
+  )
+
+  return {columns, clauses}
 }
 
-function columnize(x){
-    return x.funcs? 
-        `(${x.funcs.reduceRight((prev, curr) => `${curr}(${prev})`, varizeWithFunctions(x.name, x.funcs))} AS ${varize(x.name)})`
-        : varize(x.name)
+function columnize(x) {
+  return x.funcs
+    ? `(${x.funcs.reduceRight(
+        (prev, curr) => `${curr}(${prev})`,
+        varizeWithFunctions(x.name, x.funcs),
+      )} AS ${varize(x.name)})`
+    : varize(x.name)
 }
 
-function filterLiteral ({path, value}){
-   return pathProperties(path, `"""${value}"""`, '') 
+function filterLiteral({path, value}) {
+  return pathProperties(path, `"""${value}"""`, '')
 }
-function filterUris ({path, value}){
-   return pathProperties(path, `${value}`, '') 
+function filterUris({path, value}) {
+  return pathProperties(path, `${value}`, '')
 }
 
+const varize = x => '?' + x.replace(':', '_')
 
-const varize = x => '?' + x.replace(':','_')
+const varizeWithFunctions = (x, funcs) => varize(x) + (funcs ? '__0' : '')
 
-const varizeWithFunctions = (x, funcs) => varize(x) + (funcs? '__0' : '')
-
-
-function pathProperties (path, finalVal, lastSparql){
+function pathProperties(path, finalVal, lastSparql) {
   lastSparql = lastSparql || ''
   const len = path.length
-  if(len===0) return lastSparql
+  if (len === 0) return lastSparql
   else {
     const rest = path.slice(0, -1)
-    const curr = path[len-1]
-    const subject = len===1? '?item' : varize(path[len-2].property)
+    const curr = path[len - 1]
+    const subject = len === 1 ? '?item' : varize(path[len - 2].property)
     const object = finalVal || varize(curr.property)
-    const triple = curr.inverse? `${object} ${curr.property} ${subject}` : `${subject} ${curr.property} ${object}`
+    const triple = curr.inverse
+      ? `${object} ${curr.property} ${subject}`
+      : `${subject} ${curr.property} ${object}`
     const sparql = `${triple} .\n\t    ${lastSparql}`
-    return pathProperties(rest, false, curr.optional? `OPTIONAL {\n\t    ${sparql}\n\t}` : sparql)
+    return pathProperties(
+      rest,
+      false,
+      curr.optional ? `OPTIONAL {\n\t    ${sparql}\n\t}` : sparql,
+    )
   }
 }
-
